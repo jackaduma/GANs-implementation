59ea1d685e84afe35733574b7cce09d64587924e
Kun Ma <jackaduma@gmail.com>
2022-01-24 20:12:03 +0800

    pix2pix

---
+++ image_transfer/pix2pix/__init__.py
@@ -0,0 +1,12 @@
+#!python
+# -*- coding: utf-8 -*-
+# @author: Kun
+
+'''
+Author: Kun
+Date: 2022-01-24 19:28:58
+LastEditTime: 2022-01-24 19:28:58
+LastEditors: Kun
+Description: 
+FilePath: /projects/my_open_projects/GANs-implementation/image_transfer/pix2pix/__init__.py
+'''
---
+++ image_transfer/pix2pix/datasets.py
@@ -0,0 +1,51 @@
+#!python
+# -*- coding: utf-8 -*-
+# @author: Kun
+
+'''
+Author: Kun
+Date: 2022-01-24 19:39:31
+LastEditTime: 2022-01-24 19:39:31
+LastEditors: Kun
+Description: 
+FilePath: /projects/my_open_projects/GANs-implementation/image_transfer/pix2pix/datasets.py
+'''
+
+
+import glob
+import random
+import os
+import numpy as np
+
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root, transforms_=None, mode="train"):
+        self.transform = transforms.Compose(transforms_)
+
+        self.files = sorted(glob.glob(os.path.join(root, mode) + "/*.*"))
+        if mode == "train":
+            self.files.extend(
+                sorted(glob.glob(os.path.join(root, "test") + "/*.*")))
+
+    def __getitem__(self, index):
+
+        img = Image.open(self.files[index % len(self.files)])
+        w, h = img.size
+        img_A = img.crop((0, 0, w / 2, h))
+        img_B = img.crop((w / 2, 0, w, h))
+
+        if np.random.random() < 0.5:
+            img_A = Image.fromarray(np.array(img_A)[:, ::-1, :], "RGB")
+            img_B = Image.fromarray(np.array(img_B)[:, ::-1, :], "RGB")
+
+        img_A = self.transform(img_A)
+        img_B = self.transform(img_B)
+
+        return {"A": img_A, "B": img_B}
+
+    def __len__(self):
+        return len(self.files)
